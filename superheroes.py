be0d0ff910fa6ef8a53c8d1e81046f500fd7eefb
inv0w <jonjoninfante@gmail.com>
2019-09-23 05:44:39 -0700

    Finished Pages 1 and 2

--- superheroes.py
+++ superheroes.py
@@ -15,36 +15,91 @@ class Armor:
         self.max_block = max_block
 
     def block(self):
-        pass
+        ''' Return a random value between 0 and the initialized max_block strength. '''
+        rand_block = random.randint(0, self.max_block)
+        return rand_block
 
 class Hero:
-    def __init__(self, name, starting_health):
+    def __init__(self, name, starting_health=100):
         self.name = name
-        self.starting_health = max_block
-        max_block = 100
+        self.starting_health = starting_health
+        self.current_health = starting_health
+        self.abilities = []
+        self.armors = []
 
     def add_ability(self, ability):
-        pass
+        self.abilities.append(ability)
 
     def attack(self):
-        pass
+        '''
+        Calculate the total damage from all ability attacks.
+          return: total:Int
+        '''
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+    def add_armor(self, armor):
+        '''
+        Add armor to self.armors
+        Armor: Armor Object
+        '''
+        self.armors.append(armor)
+
 
     def defend(self, incoming_damage):
-        pass
+        '''
+        Runs `block` method on each armor.
+        Returns sum of all blocks
+        '''
+        total_blocked = 0
+        for armor in self.armors:
+            total_blocked += armor.block()
+        return total_blocked
 
     def take_damage(self, damage):
-        pass
+        '''
+        Updates self.current_health to reflect the damage minus the defense.
+        '''
+        self.current_health -= (damage - self.defend(damage))
 
     def is_alive(self):
-        pass
+        '''
+        Return True or False depending on whether the hero is alive or not.
+        '''
+        if self.current_health >= 0:
+            return True
+        else:
+            return False
 
     def fight(self, opponent):
-        pass
-
+        '''
+        Current Hero will take turns fighting the opponent hero passed in.
+        '''
+        if (len(self.abilities) + len(opponent.abilities)) < 1:
+            return 'Draw'
+        else:
+            while self.is_alive() and opponent.is_alive():
+                self.take_damage(opponent.attack())
+                opponent.take_damage(self.attack())
+        if self.is_alive():
+            print(f'{self.name} won!')
+        else:
+            print(f'{opponent.name} won!')
 
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
+
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
